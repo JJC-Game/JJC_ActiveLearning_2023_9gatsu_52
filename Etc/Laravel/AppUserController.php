03426d567ee5b7252f6ee8b1aa4ee4122f87adc0
jeremi4h <fumiya.morikuni@gmail.com>
2023-09-12 11:06:04 +0900

    play_chara_gacha関数追加

--- Etc/Laravel/AppUserController.php
+++ Etc/Laravel/AppUserController.php
@@ -68,4 +68,20 @@ class AppUserController extends Controller
         $app_user = AppUser::find($id);
         return view('app_user', ['app_user' => $app_user]);
     }
+
+    public function play_chara_gacha($id)
+    {
+        $app_user = AppUser::find($id);
+        $has_chara_flag = $app_user->has_chara_flag;
+        $result_string = "";
+
+        $rand_num = mt_rand(1, 10000);
+        $jouyo = $rand_num % 32;
+        $result_string = $result_string.$jouyo.",";
+        $jouyo_flag = 1 << $jouyo;
+        $app_user->has_chara_flag = $has_chara_flag | $jouyo_flag;
+        $app_user->save();
+
+        return view('play_chara_gacha', ['result_string' => $result_string]);
+    }
 }
