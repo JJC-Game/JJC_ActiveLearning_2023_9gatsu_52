cc7c94dc8c8092c1e58c1c3a2448fd69e85859d7
jeremi4h <fumiya.morikuni@gmail.com>
2023-09-12 10:34:05 +0900

    web.php置き換え

--- Etc/Laravel/web.php
+++ Etc/Laravel/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/app_user/{id}', [AppUserController::class, 'app_user'])->name('al.app_user');
+
 Route::get('/', function () {
     return view('welcome');
 });
